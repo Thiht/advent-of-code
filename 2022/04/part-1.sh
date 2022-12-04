1fa9697c29bbd92a198e7fe24de0380f9a94ad93
Thibaut Rousseau <thibaut.rousseau.44@gmail.com>
2022-12-04 14:24:25 +0100

    day 4

---
+++ 2022/04/part-1.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+while read -r line; do
+  first_start=$(echo "$line" | cut -d, -f1 | cut -d- -f1)
+  first_end=$(echo "$line" | cut -d, -f1 | cut -d- -f2)
+
+  second_start=$(echo "$line" | cut -d, -f2 | cut -d- -f1)
+  second_end=$(echo "$line" | cut -d, -f2 | cut -d- -f2)
+
+  if (( first_start >= second_start && first_end <= second_end )) || \
+     (( second_start >= first_start && second_end <= first_end )); then
+    echo 1
+  else
+    echo 0
+  fi
+done < input.txt | \
+paste -sd+ - | \
+bc
